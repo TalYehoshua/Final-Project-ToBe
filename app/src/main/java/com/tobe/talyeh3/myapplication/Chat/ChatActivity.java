e38d5bbc2e65d7b390d2ebb096e9a7b8c1be4662
TalYehoshua <talyeh3@gmail.com>
2019-01-15 14:54:12 +0200

    Upload Final Project To git

--- app/src/main/java/com/tobe/talyeh3/myapplication/Chat/ChatActivity.java
+++ app/src/main/java/com/tobe/talyeh3/myapplication/Chat/ChatActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -96,7 +97,7 @@ public class ChatActivity extends AppCompatActivity {
                 }
                 @Override
                 public void onCancelled(DatabaseError databaseError) {
-
+                    Log.w( "load failed", databaseError.toException());
                 }
             });
 
